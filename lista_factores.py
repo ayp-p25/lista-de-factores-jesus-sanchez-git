3ef56485b8c256fda4755206101c16acfd093543
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-03-10 23:51:40 -0600

    First

--- lista_factores.py
+++ lista_factores.py
@@ -1,15 +1,9 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+def es_factor(Num):
+    factores = []
+    for i in range(1, Num+1):
+        if Num % i == 0:
+            factores.append(i)
+    return factores
+Num = int(input("Escriba el numero del que quisiera saber sus factores: \n"))
+factores = es_factor(Num)
+print(f"Los factores de {Num} son : {factores}")
